d48ae9545580146ba194d552fa50a20548146302
RaJharit77 <hei.rajo.2@gmail.com>
2024-09-03 14:17:10 +0300

    feat: remove a feature

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import json from '@rollup/plugin-json';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), json()],
+  plugins: [react()],
   resolve: {
     alias: {
       fs: 'browserify-fs',

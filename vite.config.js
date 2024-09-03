7acd8fbfe47ff2e5a477d7b4eaa5c49ead4b352f
RaJharit77 <hei.rajo.2@gmail.com>
2024-09-03 13:46:33 +0300

    feat: add a new feature

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+import json from '@rollup/plugin-json';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), json()],
   resolve: {
     alias: {
       fs: 'browserify-fs',

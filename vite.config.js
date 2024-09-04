da6e2a5d2e661ea9fba60254de6a05f8c65e2d5b
RaJharit77 <hei.rajo.2@gmail.com>
2024-09-04 13:09:15 +0300

    feat: new feature added

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,3 @@
-/*import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      fs: 'browserify-fs',
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  root: './ui',
-  build: {
-    outDir: 'dist',
-  }
-});*/
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
@@ -38,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://patrimoine-economique-3kl2.onrender.com',
         changeOrigin: true,
         secure: false,
       },

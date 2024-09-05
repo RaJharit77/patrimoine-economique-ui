dbe4791d0015036e65bcd51083ff0ded92dd2756
RaJharit77 <hei.rajo.2@gmail.com>
2024-09-05 12:37:10 +0300

    fix: path error

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
         target: 'https://patrimoine-economique-backend.onrender.com',
         changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   }

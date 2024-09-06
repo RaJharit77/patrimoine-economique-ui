2a995dc3ae347da35871ef6bd6258819306414d9
RaJharit77 <hei.rajo.2@gmail.com>
2024-09-06 10:01:30 +0300

    feat: feature added

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
       fs: 'browserify-fs',
@@ -11,6 +14,7 @@ export default defineConfig({
     },
   },
   server: {
+    port: 5000,
     proxy: {
       '/api': {
         target: 'https://patrimoine-economique-backend.onrender.com',
